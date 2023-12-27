fde8ce4f2ff595a2130fd2e2045bf817ce69cd38
lc <1641767724@qq.com>
2023-12-27 22:07:01 +0800

    upload

---
+++ generated/models/InterfaceInfoUpdateRequest.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type InterfaceInfoUpdateRequest = {
+    description?: string;
+    id?: number;
+    method?: string;
+    name?: string;
+    requestHeader?: string;
+    requestParams?: string;
+    responseHeader?: string;
+    status?: number;
+    url?: string;
+};
