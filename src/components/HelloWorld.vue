66b282512d51534b414af3bfe28f684c40801dec
lc <1641767724@qq.com>
2023-12-27 21:44:57 +0800

    upload

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,38 @@
 
 <template>
-  <div>hello-world</div>
+  <a-upload-dragger
+      name="file"
+      :multiple="true"
+      action="https://localhost:7529/api/file/upload"
+      @change="handleChange"
+  >
+    <p class="ant-upload-drag-icon">
+      <a-icon type="inbox" />
+    </p>
+    <p class="ant-upload-text">
+      Click or drag file to this area to upload
+    </p>
+    <p class="ant-upload-hint">
+      Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+      band files
+    </p>
+  </a-upload-dragger>
 </template>
 
 <script setup lang="ts">
-
-defineProps<{ msg: string }>()
-
+  const handleChange = (info) => {
+        const status = info.file.status;
+        if (status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (status === 'done') {
+          this.$message.success(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+          this.$message.error(`${info.file.name} file upload failed.`);
+        }
+  }
 </script>
+
 <style scoped>
 
 </style>
